70f5705910ad649a0746f5e84d16c27e921e866e
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-04-17 01:08:18 +0600

    Navigate useLocation and Module Summary

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,10 +1,13 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UserDetails2 from '../userDetails2/UserDetails2';
 
 const User = ({user}) => {
 
     const [showInfo, setShowInfo] = useState(false);
+    const [visitHome, setVisitHome] = useState(false);
+    const loacation = useLocation();
+    console.log(location);
 
     const {id, name, email, phone} = user;
 
@@ -16,6 +19,11 @@ const User = ({user}) => {
         padding: '10px',
         margin: '10px'
     }
+
+ if(visitHome){
+    return <Navigate to='/'></Navigate>
+ }
+
     return (
         <div style={userStyle}>
             <h3>{name}</h3>
@@ -29,6 +37,7 @@ const User = ({user}) => {
                   <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
              }
+             <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
